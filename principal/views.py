072a6b5d1cf5281f528d3cbc6d7ada9bdaa97467
rafasaal88 <rafasaal88@gmail.com>
2015-08-13 17:49:46 +0200

    RC2

--- principal/views.py
+++ principal/views.py
@@ -2,15 +2,13 @@
 # coding=utf-8
 from django.contrib.auth.hashers import make_password
 
-
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate, logout, get_user_model
 from django.contrib.auth.decorators import login_required
 
